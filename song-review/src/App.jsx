9b0bc77dd3c7476396386a68caaf7c27aaeffce7
Prashant-koi <koiralaprashanta10@gmail.com>
2024-12-17 13:10:51 -0500

    Started on hero section

--- song-review/src/App.jsx
+++ song-review/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import Navbar from './components/navbar'
+import HeroSection from './components/herosection'
 
 export default function App() {
   return (
-    
+    <>
       <Navbar/>
+      
+        <HeroSection />
+      
+      </>
 
     
   )
---
+++ song-review/src/components/herosection.jsx
@@ -0,0 +1,9 @@
+function HeroSection() {
+    return(
+        <div>
+            <h1 className=" text-center font-bold"> This is the hero Section. This is gonna contain song recommendations and genre of songs the icon of genre's will be the top rated song of the genre</h1>
+        </div>
+    )
+}
+
+export default HeroSection;
