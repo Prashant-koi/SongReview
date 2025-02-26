36ac29d56bb29859693e2f30e4e3b0d88385916a
Prashant-koi <koiralaprashanta10@gmail.com>
2025-02-26 13:25:17 -0600

    Add: Set up routes and started working on Login functionality

--- song-review/src/App.jsx
+++ song-review/src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import BrowserRouter, { Routes } from 'react-router-dom'
 import Navbar from './components/navbar'
 import HeroSection from './components/herosection'
+import Layout from './components/Layout'
 
 export default function App() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element = {<Layout />} />
+      <Route index element = {<HeroSection />} />
+    </Routes>
+    </BrowserRouter>
       <Navbar/>
       
         <HeroSection />
---
+++ song-review/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React  from "react";
+export default function Login() {
+    return (
+        <div>
+            <button className="block px-4 py-2 bg-gradient-to-r from-pink-400 to-pink-800 rounded">
+                Login With Google
+            </button>
+        </div>
+    )
+}
